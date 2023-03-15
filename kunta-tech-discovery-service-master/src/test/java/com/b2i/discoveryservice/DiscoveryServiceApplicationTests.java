3b01900dc97525b60c343b2518612709f394b65c
nguepempoue <nguepempouejulio@gmail.com>
2023-03-15 11:57:05 +0100

    version de deploiement

---
+++ kunta-tech-discovery-service-master/src/test/java/com/b2i/discoveryservice/DiscoveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.b2i.discoveryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
